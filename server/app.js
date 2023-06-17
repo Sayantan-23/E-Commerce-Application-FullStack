07f2b2efd5fbbdb69accfc43a5f62200a28833e0
Sayantan <sayantan358@gmail.com>
2023-06-17 20:58:25 +0530

    order added

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 import express from "express"
+import cookieParser from "cookie-parser"
 // Route Imports
 import productRoute from "./src/routes/productRoute.js"
 import userRoute from "./src/routes/userRoute.js"
+import orderRoute from "./src/routes/orderRoute.js"
 
 import errorMiddleWare from "./src/middlewares/error.js"
-
-import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(express.json())
@@ -13,6 +13,7 @@ app.use(cookieParser())
 
 app.use("/api/v1", productRoute)
 app.use("/api/v1", userRoute)
+app.use("/api/v1", orderRoute)
 
 app.use(errorMiddleWare)
 
